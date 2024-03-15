6a30e7345f237d0c638c4b7b97a9e47c47e99de4
yzhkah <nguyennkhanhvy@dtu.edu.vn>
2024-03-15 10:46:07 +0700

    submit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App m-9">
+     <h1 className="text-center p-2 text-black text-4xl font-bold "> Bài Tập Css Cho Text </h1>
+      <div className="text-xl font-bold p-2 text-black"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi libero quod, dolore veniam repellendus iste earum a ipsam numquam, porro fuga ut vero quam cum. Eligendi odit est voluptates neque. </div>
+      <div className="text-2xl font-bold tetx-black p-2"> Heading 1 </div>
+      <div className="text-xl p-2"> <span class="font-bold">Lorem ipsum </span> dolor sit amet consectetur adipisicing elit. Ipsum, amet. Omnis exercitationem voluptates laboriosam ea explicabo? Asperiores accusamus iusto assumenda sit, similique deleniti repellat, tempore perspiciatis, debitis officia vitae earum.</div>
+      <div className="text-xl p-2"> Ornare arcu odio ut sem nulla pharetra diam sit amet. Facilisis gravida neque convallis a cras semper. Massa sapien faucibus et molestie ac. Id venenatis a condimentum vitae sapien pellentesque habitant. Leo duis ut diam quam nulla porttitor massa. </div>
+      <div className="text-3xl font-bold tetx-black p-2"> Heading 2</div>
+      <div className="text-xl p-2"> <span class="line-through"> Lorem ipsum </span> , dolor sit amet consectetur adipisicing elit. Dolorum molestias necessitatibus placeat earum dolorem veniam veritatis corrupti rerum facilis aliquid provident pariatur reprehenderit, officia eos molestiae voluptatibus similique nostrum doloribus?Vitae suscipit tellus mauris a diam. Vitae turpis massa sed elementum tempus egestas sed sed. Consectetur adipiscing elit ut aliquam purus sit. Mus mauris vitae ultricies leo integer. Et sollicitudin ac orci phasellus egestas tellus rutrum. Leo vel orci porta non pulvinar neque laoreet. Mi tempus imperdiet nulla malesuada pellentesque elit eget gravida. Blandit volutpat maecenas volutpat blandit aliquam. Dictum non consectetur a erat nam at lectus urna. In tellus integer feugiat scelerisque varius. Est sit amet facilisis magna etiam tempor orci eu. Lectus arcu bibendum at varius vel pharetra.</div>
+      <div className="text-xl text-yellow-400 text-right"> Phan Van Cuong - Fouder Unitop </div>
     </div>
   );
 }
-
 export default App;
